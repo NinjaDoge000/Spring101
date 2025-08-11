86489bb5cfbe018d764ffd1296d70b0644da5b32
NinjaDoge <ninjadoge000@gmail.com>
2025-08-11 20:31:33 +0000

    @springbootapplication

---
+++ src/main/java/com/ninjagedoge/spring101/Spring101.java
@@ -0,0 +1,11 @@
+package com.ninjagedoge.spring101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring101 {
+    public static void main(String args[]) {
+        SpringApplication.run(Spring101.class, args);
+    }
+}

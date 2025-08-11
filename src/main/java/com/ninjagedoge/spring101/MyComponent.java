284456cac6ecb64ce617d060eee1ef1905e8ec96
NinjaDoge <ninjadoge000@gmail.com>
2025-08-11 20:54:46 +0000

    @Component

---
+++ src/main/java/com/ninjagedoge/spring101/MyComponent.java
@@ -0,0 +1,11 @@
+package com.ninjagedoge.spring101;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyComponent {
+
+    public void sayHello() {
+        System.out.println("hello from MyComponent class");
+    }
+}
--- src/main/java/com/ninjagedoge/spring101/Spring101.java
+++ src/main/java/com/ninjagedoge/spring101/Spring101.java
@@ -2,10 +2,14 @@ package com.ninjagedoge.spring101;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring101 {
     public static void main(String args[]) {
-        SpringApplication.run(Spring101.class, args);
+        ApplicationContext context = SpringApplication.run(Spring101.class, args);
+
+        MyComponent myComponent = context.getBean(MyComponent.class);
+        myComponent.sayHello();
     }
 }
